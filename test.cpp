c21a4ed38dbdf327f4ad28cfb807e2948cc01279
yerralaxmi <hiyerra@gmail.com>
2025-10-17 19:28:53 +0530

    Squashed
    m1
    
    m2
    
    m3
    
    m4
    
    m5
    
    test copy
    
    comment added
    
    cherry pick from yoga1
    
    commented out.
    
    yoga1: c2
    
    1st line updated in yoga2 branch
    
    1st line updated in main branch
    
    yoga2: update
    
    main: update

---
+++ test.cpp
@@ -0,0 +1,21 @@
+//test.cpp
+#include <iostream>
+using namespace std; //namespace of std
+
+int main() 
+{
+    // Write C++ code here
+    cout<<"Hello world"<<endl;
+    cout<<"Hi world"<<endl;
+    cout<<"m1"<<endl; //m1
+    cout<<"m2"<<endl; //m2
+    cout<<"m3"<<endl; //m3
+    cout<<"m4"<<endl; //m4
+    cout<<"m5"<<endl; //m5
+    //yoga1: c1
+    //yoga1: c2
+    //yoga2: c3
+
+    return 0;
+}
+//end
