c68e78558c49f7e356c17fdfb6bc252b4f8cb374
yerralaxmi <hiyerra@gmail.com>
2025-10-17 19:28:53 +0530

    Squashed

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Write C++ code here
+    cout<<"Hello world"<<endl;
+    cout<<"Hi world"<<endl;
+    cout<<"m1"<<endl;
+    cout<<"m2"<<endl;
+    cout<<"m3"<<endl;
+    cout<<"m4"<<endl;
+    cout<<"m5"<<endl;
+
+    return 0;
+}
