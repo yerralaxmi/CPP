430f2dc1c0b8e2b09ef0b9d809363eefe6e05348
yerralaxmi <hiyerra@gmail.com>
2025-10-18 11:44:23 +0530

    m1

--- test.cpp
+++ test.cpp
@@ -1,9 +1,10 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
+using namespace std;
 
 int main() {
     // Write C++ code here
-    std::cout << "Hell world";
+    cout << "Hello world"<<endl;
 
     return 0;
 }
