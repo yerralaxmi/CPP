38a7cd5c4768e9cbb553dc9d48a86d35cfec82c4
yerralaxmi <hiyerra@gmail.com>
2025-10-18 13:08:19 +0530

    b3

--- test.cpp
+++ test.cpp
@@ -7,8 +7,9 @@ int main()
     // Write C++ code here
     cout << "Hello world"<<endl;
     cout <<"Hi world"<<endl;
-    cout <<"1st change in br1";
-    cout <<"2nd change in br1";
+    cout <<"1st change in br1"<<endl;
+    cout <<"2nd change in br1"<<endl;
+    cout <<"3rd change in br1"<<endl;
 
     return 0;
 }
