61128b355a8f8fd1e93a3c6c98b62fd78d032839
yerralaxmi <hiyerra@gmail.com>
2025-10-19 11:28:01 +0530

    m1

--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@ int main()
     // Write C++ code here
     cout<<"Hello world"<<endl;
     cout<<"Hi world"<<endl;
-    cout<<"m1"<<endl;
+    cout<<"m1"<<endl; //m1
     cout<<"m2"<<endl;
     cout<<"m3"<<endl;
     cout<<"m4"<<endl;
