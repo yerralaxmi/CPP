71a7846dde65f3524d8731732e3ac1a4645ac8f0
yerralaxmi <hiyerra@gmail.com>
2025-10-19 11:28:25 +0530

    m2

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@ int main()
     cout<<"Hello world"<<endl;
     cout<<"Hi world"<<endl;
     cout<<"m1"<<endl; //m1
-    cout<<"m2"<<endl;
+    cout<<"m2"<<endl; //m2
     cout<<"m3"<<endl;
     cout<<"m4"<<endl;
     cout<<"m5"<<endl;
